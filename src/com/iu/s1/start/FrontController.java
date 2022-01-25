0858b4cb032b184a36afdd7748810debb5a25ed7
Daeki <kimdaeki@gmail.com>
2022-01-25 16:51:52 +0900

    1

--- src/com/iu/s1/start/FrontController.java
+++ src/com/iu/s1/start/FrontController.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import com.iu.s1.department.DepartMentController;
 import com.iu.s1.employee.EmployeeController;
 import com.iu.s1.location.LocationController;
+import com.iu.s1.region.RegionController;
 
 public class FrontController {
 	
@@ -14,6 +15,8 @@ public class FrontController {
 	private LocationController locationController;
 	// EmployeeController
 	private EmployeeController employeeController;
+	//RegionController
+	private RegionController regionController;
 	
 	public FrontController() {
 		sc= new Scanner(System.in);
@@ -22,6 +25,8 @@ public class FrontController {
 		locationController = new LocationController();
 		// EmployeeController
 		employeeController = new EmployeeController();
+		//RegionController
+		regionController = new RegionController();
 	}
 	
 	
@@ -32,7 +37,8 @@ public class FrontController {
 			System.out.println("1. 직 원  관 리");
 			System.out.println("2. 부 서  관 리");
 			System.out.println("3. 지 역  관 리");
-			System.out.println("4. 프로그램 종료");
+			System.out.println("4. 대 륙  관 리");
+			System.out.println("5. 프로그램 종료");
 			int select = sc.nextInt();
 			switch (select) {
 			case 1:
@@ -45,6 +51,9 @@ public class FrontController {
 			case 3:
 				// locationController의 start 메서드 호출
 				locationController.start();
+				break;
+			case 4:
+				regionController.start();
 				break;
 			default:
 				flag = false;
